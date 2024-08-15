5ac08b69d519ce7020adbc7be55c7f796cb4e8c9
mariacoutinhoo <131712076+mariacoutinhoo@users.noreply.github.com>
2024-08-15 16:48:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [
     {
         enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1 ","Alternativa 2 "]
+        alternativas: ["Alternativa 1","Alternativa 2"]
     },
     {
         enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1 ","Alternativa 2 "]
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["Alternativa 1","Alternativa 2"]
     }
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual
+}
